cb603850d8301910406d2ec87c72a1259b241bcd
wang qiang <wqpod2g@gmail.com>
2014-10-27 14:15:47 +0800

    1.1

--- src/nju/iip/clustering/KMeans.java
+++ src/nju/iip/clustering/KMeans.java
@@ -36,6 +36,8 @@ public class KMeans {
 	 */
 	private static ArrayList<ArrayList<Double>>allMatrix=new ArrayList<ArrayList<Double>>();
 	
+	
+	private static ArrayList<Integer>classAricleNum=new ArrayList<Integer>();
 	/**
 	 * @description 获得整个样本的特征向量
 	 * @return Map<String,ArrayList<ArrayList<Double>>>allVector
@@ -46,7 +48,9 @@ public class KMeans {
 		List<String>fileList=Tools.readDirs(SamplePath);
 		Double ID=1.0;
 		for(String article:fileList){
-			allMatrix.addAll(Tools.getOneClassVector(article,ID));
+			ArrayList<ArrayList<Double>>OneClassVector=Tools.getOneClassVector(article,ID);
+			allMatrix.addAll(OneClassVector);
+			classAricleNum.add(OneClassVector.size());
 			ID++;
 		}
 		
@@ -59,9 +63,11 @@ public class KMeans {
 	 * @return meanVectors
 	 */
 	public static ArrayList<ArrayList<Double>>initializeMeanVectors(){
+		int sum=0;
 		for(int i=0;i<10;i++){
-			int num=new Random().nextInt(allMatrix.size());
-			meanVectors.add(allMatrix.get(num));
+			int num=new Random().nextInt(classAricleNum.get(i));
+			meanVectors.add(allMatrix.get(num+sum));
+			sum=sum+classAricleNum.get(i);
 			//System.out.println(num);
 		}
 		return meanVectors;
@@ -100,7 +106,7 @@ public class KMeans {
 	}
 	
     /**
-     * @description 对当前mean进行分类
+     * @description 将每个点进行分类
      * @return clusterMap
      */
 	public static HashMap<Integer,ArrayList<ArrayList<Double>>>getClusterMap(){
@@ -118,7 +124,7 @@ public class KMeans {
 	 */
 	public static ArrayList<Double> getMeanVectors(ArrayList<ArrayList<Double>>x){
 		ArrayList<Double>meanVector=new ArrayList<Double>();
-		for(int i=0;i<keyWordsNum-1;i++){
+		for(int i=0;i<keyWordsNum;i++){
 			Double sum=0.0;
 			for(int j=0;j<x.size();j++){
 				sum=sum+x.get(j).get(i);
@@ -131,6 +137,86 @@ public class KMeans {
 	}
 	
 	
+	/**
+	 * @description 计算I(omega,c)
+	 * @return
+	 */
+	public static Double getI(){
+		Double N=(double) allMatrix.size();
+		
+		Double I=0.0;
+		for(int i=0;i<10;i++){
+			ArrayList<ArrayList<Double>>oneCluster=clusterMap.get(i);
+			int w=clusterMap.get(i).size();
+			if(w==0){
+				w=w+1;
+			}
+			//System.out.println(w);
+			for(int j=0;j<10;j++){
+				Double n=0.0;
+				for(int k=0;k<oneCluster.size();k++){
+					if(oneCluster.get(k).get(keyWordsNum)==j){
+						n++;
+					}
+				}
+				if(n==0){
+					n=n+0.1;
+				}
+				I=I+(n/N)*(Math.log10((N*n)/(w*classAricleNum.get(j))));
+			}
+		}
+		return I;
+	}
+	
+	/**
+	 * @description 计算H(omega)
+	 * @return
+	 */
+	public static Double getHOmega(){
+		Double HOmega=0.0;
+		Double N=(double) allMatrix.size();
+		for(int i=0;i<10;i++){
+			double w=clusterMap.get(i).size();
+			if(w==0){
+				w=w+0.1;
+			}
+			HOmega=HOmega+(w/N)*Math.log10(w/N);
+		}
+		return -HOmega;
+	}
+	
+	
+	public static Double getHC(){
+		Double HC=0.0;
+		Double N=(double) allMatrix.size();
+		for(int i=0;i<10;i++){
+			int c=classAricleNum.get(i);
+			HC=HC+(c/N)*Math.log10(c/N);
+		}
+		return -HC;
+		
+	}
+	
+	 /**
+     * @decription 计算NMI
+     * @return
+     */
+	public static Double getNMI(){
+		
+		Double I=getI();
+		Double HOmega=getHOmega();
+		Double HC=getHC();
+		System.out.println("HOmega="+HOmega);
+		System.out.println("HC="+HC);
+		System.out.println("I="+I);
+		Double NMI=2*I/(HOmega+HC);
+		return NMI;
+	}
+	
+	
+	/**
+	 * @description 整个处理过程
+	 */
 	public static void process(){
 		meanVectors=initializeMeanVectors();
 		for(int n=0;n<10;n++){
@@ -142,20 +228,15 @@ public class KMeans {
 			}
 			
 		}
-		
+		System.out.println("K-Means的NMI值为:"+getNMI());
 		
 	}
 	
-	
-	
-	
 	public static void main(String[] args) throws FileNotFoundException, IOException{
+		
 		getAllMatrix();
 		process();
-	    System.out.println(clusterMap.size());
-	    for(int i=0;i<clusterMap.size();i++){
-	    	 System.out.println(clusterMap.get(i).size());
-	    }
+
 	}
 
 }
