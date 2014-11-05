4cd4a17bd44ccbc459af7e157f6b1438623325b0
wang qiang <wqpod2g@gmail.com>
2014-11-05 22:14:01 +0800

    1.3.2

--- src/nju/iip/clustering/KMeans.java
+++ src/nju/iip/clustering/KMeans.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Random;
 
 import nju.iip.preprocess.Tools;
@@ -241,8 +240,7 @@ public class KMeans {
 		
 		getAllMatrix();
 		process();
-		
-
 	}
+
 
 }
